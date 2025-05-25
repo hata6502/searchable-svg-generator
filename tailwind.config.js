43ad55ae2ceb3e30b57d149b9f6364abab8541c7
hata6502 <7702653+hata6502@users.noreply.github.com>
2025-05-25 20:26:34 +0900

    tailwindcss v4

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["docs/index.html", "docs/doc.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
